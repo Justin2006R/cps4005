df3849f352b81074c49dccd73775818d339123af
2403851 <2403851@1BR7ZW3.simms>
2025-01-27 12:24:06 +0000

    Using variables with numbers and words

---
+++ labs/src/main/java/com/justin/labs/Labs.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.justin.labs;
+
+/**
+ *
+ * @author 2403851
+ */
+public class Labs {
+
+    public static void main(String[] args) {
+        
+        double age = 18.5;
+        String name = "Justin";
+        
+        System.out.println("Welcome to basics");
+        System.out.println("Name: " + name);
+        System.out.println("Age" + age);
+    }
+}
