0e7bba652320543c761f33a81ec57816fba8cb69
2403851 <2403851@1BR7ZW3.simms>
2025-02-03 11:41:40 +0000

    Car class get/set

---
+++ labs/src/main/java/com/justin/labs/Car.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.justin.labs;
+
+/**
+ *
+ * @author 2403851
+ */
+public class Car {
+    private String colour;
+    private String make;
+    private String model;
+    private int year;
+    private int price;
+    
+    public Car(String colour, String make, String model, int year, int price){
+        this.colour = colour;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+    
+    public String getColour(){
+        return colour;
+    }
+    
+    public void setColour(String newColour){
+        colour = newColour;
+    }
+    
+    public String getMake(){
+        return make;
+    }
+    
+    public void setMake(String newMake){
+        make = newMake;
+    }
+    
+    public String getModel(){
+        return model;
+    }
+    
+    public void setModel(String newModel){
+        model = newModel;
+    }
+    
+    public int getYear(){
+        return year;
+    }
+    
+    public void setYear(int newYear){
+        year = newYear;
+    }
+    
+    public int getPrice(){
+        return price;
+    }
+    
+    public void setPrice(int newPrice){
+        price = newPrice;
+    }
+}
+
+
