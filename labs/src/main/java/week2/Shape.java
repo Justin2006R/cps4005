674893aba5b8cfab8d79f741f89ecc9c27513c6f
2403851 <2403851@1BR7ZW3.simms>
2025-02-07 11:32:17 +0000

    Shape class creation

---
+++ labs/src/main/java/week2/Shape.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week2;
+
+/**
+ *
+ * @author 2403851
+ */
+public class Shape {
+    
+    public void draw(){
+        System.out.println("Drawing Shape");
+    }
+}
