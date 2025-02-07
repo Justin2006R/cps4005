4d2dd7138434de451e081b7eb3c7d71547af4795
2403851 <2403851@1BR7ZW3.simms>
2025-02-07 11:31:51 +0000

    InheritanceExample class creation

---
+++ labs/src/main/java/week2/InheritanceExample.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week2;
+
+/**
+ *
+ * @author 2403851
+ */
+public class InheritanceExample {
+    
+    public static void main(String[] args){
+        
+        Circle myCircle =  new Circle();
+        
+        myCircle.draw();
+        myCircle.calculateArea();
+        
+    }
+    
+}
