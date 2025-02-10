e6645ac2fc81ddafeb52a896dbcb298df0ec651a
2403851 <2403851@1BR7ZW3.simms>
2025-02-10 11:46:04 +0000

    overriding email service

---
+++ labs/src/main/java/week2part2/SecureEmailService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week2part2;
+
+/**
+ *
+ * @author 2403851
+ */
+public class SecureEmailService extends EmailService {
+    @Override
+    public void printInfo(){
+        System.out.println("Sending encrypted emails with echanced security");
+    }
+    
+    
+}
